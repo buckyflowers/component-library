fba4a97843bab1620bbeeb42836fc0f1fb77d7e7
Jayden Anderson <jaydencanderson@gmail.com>
2013-07-23 11:03:33 -0600

    Starting to refactor dropdown.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-  $('textarea').autoGrow();
+  // $('textarea').autoGrow();
 });
 
+$(document).on("click", function(){
+  $(this).find('.dropdown__menu').hide();
+  $(".js-dropdown").parent().removeClass('active');
+});
+
+$(document).on("click", ".js-dropdown", function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $this_menu = $(this).closest(".js-dropdown").parent().find('.dropdown__menu');
+  $this_menu.toggle();
+  $('.dropdown__menu').not($this_menu).hide();
+  $('.dropdown__menu').not($this_menu).parent().removeClass('is-active');
+  $(this).closest(".js-dropdown").parent().toggleClass('is-active');
+});
